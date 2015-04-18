d77c4c7265d14641ed7f3908f9819c960d5c91d5
kntroh <knt.roh@gmail.com>
2015-04-18 09:49:31 +0900

    Adds test for V4 Bitmap.

--- test.d
+++ test.d
@@ -1,15 +1,22 @@
 
 import org.eclipse.swt.all;
+import java.io.ByteArrayInputStream;
 
 void main() {
 	auto d = new Display;
 	auto s = new Shell(d);
 	s.setLayout(new FillLayout);
-	s.setSize(50, 50);
+	s.setSize(100, 100);
 
 	auto l = new Label(s, SWT.NONE);
-	auto img = new Image(d, new ImageData("test.bmp"));
-	l.setImage(img);
+	auto bin = new ByteArrayInputStream(cast(byte[])std.file.read("test_rle4.bmp"));
+	auto img1 = new Image(d, new ImageData(bin));
+	l.setImage(img1);
+
+	l = new Label(s, SWT.NONE);
+	bin = new ByteArrayInputStream(cast(byte[])std.file.read("test_v4.bmp"));
+	auto img2 = new Image(d, new ImageData(bin));
+	l.setImage(img2);
 
 	s.open();
 
@@ -17,6 +24,7 @@ void main() {
 		if (!d.readAndDispatch())
 			d.sleep();
 
-	img.dispose();
+	img1.dispose();
+	img2.dispose();
 	d.dispose();
 }
